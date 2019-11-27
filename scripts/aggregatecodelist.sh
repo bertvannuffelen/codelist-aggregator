4b77daeebc997cf6ceb3f4786a6526f9f3ca94ce
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2019-11-27 13:05:20 +0100

    updated scripts

--- scripts/aggregatecodelist.sh
+++ scripts/aggregatecodelist.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+#
+# 
 
 set -x
 
 # ensure jsonld is found
 export PATH=~/.npm-global/bin:$PATH 
 
+SOURCEDIR=$HOME/project/codelist
+if [$# -eq 1] ; then 
+  if [ -n $1 ] ; then
+     SOURCEDIR=$HOME/project/$1
+  fi
+fi
+
 
 WORKDIR=/tmp/workspace/
-SOURCEDIR=$WORKDIR/source
 TARGETDIR=$WORKDIR/target
+
+export SOURCEDIR=$SOURCEDIR
+export TARGETDIR=$TARGETDIR
 
 mkdir -p $TARGETDIR
 
